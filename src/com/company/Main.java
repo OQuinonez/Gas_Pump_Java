55e7c7ff3aa26b5d5cdc7e33078d49f199137819
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-02-02 11:09:26 -0600

    Made a new branch that allows the manager to put his password in order to see transactions, inventory, income, and to refill tanks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.out.println("2.\tPay After");
     }
 
-    public static String whatTypeGas(Integer type){
+    public static String whatTypeGas(Integer type) {
         String gastype = "";
         if (type.equals(87)) {
             gastype = "Regular";
@@ -24,7 +24,8 @@ public class Main {
             gastype = "Mid-Grade";
         } else if (type.equals(93)) {
             gastype = "Premium";
-        }return gastype;
+        }
+        return gastype;
     }
 
     public static ArrayList<Converting> loadInventoryInFile() throws IOException {
@@ -40,7 +41,7 @@ public class Main {
         String[] MidList = br.readLine().toString().split(", ");
         String[] PremiumList = br.readLine().toString().split(", ");
 
-        Converting Regular =  new Converting(RegularList[0], Double.parseDouble(RegularList[1]), Double.parseDouble(RegularList[2]) );
+        Converting Regular = new Converting(RegularList[0], Double.parseDouble(RegularList[1]), Double.parseDouble(RegularList[2]));
         Converting MidGrade = new Converting(MidList[0], Double.parseDouble(MidList[1]), Double.parseDouble(MidList[2]));
         Converting Premium = new Converting(PremiumList[0], Double.parseDouble(PremiumList[1]), Double.parseDouble(PremiumList[2]));
 
@@ -63,34 +64,34 @@ public class Main {
     public static void saveInventoryInFile(ArrayList<Converting> inventory) throws IOException {
         FileWriter writer = new FileWriter("/home/basecamp/IdeaProjects/GasPump/src/com/company/inventory.txt");
         writer.write("Number, Amount-of-Money, Gallons" + "\n");
-        writer.write(inventory.get(0).typeOfGas + ", " + inventory.get(0).gallons + ", " +  inventory.get(0).amountMoney + "\n" );
+        writer.write(inventory.get(0).typeOfGas + ", " + inventory.get(0).gallons + ", " + inventory.get(0).amountMoney + "\n");
         writer.write(inventory.get(1).typeOfGas + ", " + inventory.get(1).gallons + ", " + inventory.get(1).amountMoney + "\n");
         writer.write(inventory.get(2).typeOfGas + ", " + inventory.get(2).gallons + ", " + inventory.get(2).amountMoney);
         writer.close();
     }
 
     public static void updateTransactions(String gastype, double printGallons, double printCost) throws IOException {
-        FileWriter writer = new FileWriter("/home/basecamp/IdeaProjects/GasPump/src/com/company/transactions.txt",true);
+        FileWriter writer = new FileWriter("/home/basecamp/IdeaProjects/GasPump/src/com/company/transactions.txt", true);
         writer.write("\n" + gastype + ", " + printGallons + ", " + printCost);
         writer.close();
     }
 
-    public static void updatingInventory(String gasType, Double money, Double gallons) throws IOException{
+    public static void updatingInventory(String gasType, Double money, Double gallons) throws IOException {
         ArrayList<Converting> inventory = loadInventoryInFile();
-        if (gasType.equals("Regular")){
+        if (gasType.equals("Regular")) {
             inventory.get(0).gallons += gallons;
             inventory.get(0).amountMoney -= money;
-        }else if (gasType.equals("Mid-Grade")){
-            inventory.get(1).amountMoney += money;
-            inventory.get(1).gallons -= gallons;
-        }else if (gasType.equals("Premium")){
-            inventory.get(2).amountMoney += money;
-            inventory.get(2).gallons -= gallons;
+        } else if (gasType.equals("Mid-Grade")) {
+            inventory.get(1).gallons += gallons;
+            inventory.get(1).amountMoney -= money;
+        } else if (gasType.equals("Premium")) {
+            inventory.get(2).gallons += gallons;
+            inventory.get(2).amountMoney -= money;
         }
         saveInventoryInFile(inventory);
     }
 
-    public static void displayCustomerInfo (String gasType, Double gallons, Double money) {
+    public static void displayCustomerInfo(String gasType, Double gallons, Double money) {
         System.out.println("Type of Gas Pumped: " + gasType);
         System.out.println("Total Gallons: " + gallons);
         System.out.println("Total Paid: " + money);
@@ -99,37 +100,58 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        greetings();
-        System.out.println("Which one would you like?   (Please Choose a Number)");
-        Integer type = scan.nextInt();
-        payType();
-        Integer paymentType = scan.nextInt();
+        System.out.println("If you are the manager, please type in your password \nIf you are a customer, press 1.  ");
+        String MorC = scan.nextLine();
+        if (MorC.equals("Everest1953")) {
+            System.out.println("1 - View Gas Tank\n2 - View Refill Gas Tanks\n3 - Store Income\n4 - View Transactions\n5 - Exit");
+            String choice = scan.next();
+            // Manager.viewTransactions();
+            // Manager.viewInventory();
+            // Manager.viewIncome();
+            // Manager.refillTanks();
+            if (choice.equals("1")) {
+                Manager.viewInventory();
+            } else if (choice.equals("2")) {
+                Manager.refillTanks();
+            } else if (choice.equals("3")) {
+                Manager.viewIncome();
+            } else if (choice.equals("4")) {
+                Manager.viewTransactions();
+            } else if (choice.equals("5")) {
+                System.exit(0);
+            }
+        }else if (MorC.equals("1")){
+                greetings();
+                System.out.println("Which one would you like?   (Please Choose a Number)");
+                Integer type = scan.nextInt();
+                payType();
+                Integer paymentType = scan.nextInt();
 
-        if (paymentType.equals(1)) {
-            String gasType = whatTypeGas(type);
-            System.out.println("How much would you like to put? ");
-            Double money = scan.nextDouble();
-            Converting displayGallons = new Converting(gasType, 0.0, money);
-            Double totalGals = displayGallons.prePay(money, type);
+                if (paymentType.equals(1)) {
+                    String gasType = whatTypeGas(type);
+                    System.out.println("How much would you like to put? ");
+                    Double money = scan.nextDouble();
+                    Converting displayGallons = new Converting(gasType, 0.0, money);
+                    Double totalGals = displayGallons.prePay(money, type);
 
-            displayCustomerInfo(gasType, totalGals, money);
+                    displayCustomerInfo(gasType, totalGals, money);
 
-            updateTransactions(gasType, totalGals, money);
+                    updateTransactions(gasType, totalGals, money);
 
-            updatingInventory(gasType, totalGals, money);
-        }
-        else if (paymentType.equals(2)){
-            String gasType = whatTypeGas(type);
-            System.out.println("How many gallons would you like? ");
-            Double gallons = scan.nextDouble();
-            Converting displayMoney = new Converting(gasType, gallons, 0.0);
-            Double totalMoney = displayMoney.payAfter(gallons, type);
+                    updatingInventory(gasType, totalGals, money);
+                } else if (paymentType.equals(2)) {
+                    String gasType = whatTypeGas(type);
+                    System.out.println("How many gallons would you like? ");
+                    Double gallons = scan.nextDouble();
+                    Converting displayMoney = new Converting(gasType, gallons, 0.0);
+                    Double totalMoney = displayMoney.payAfter(gallons, type);
 
-            displayCustomerInfo(gasType, gallons, totalMoney);
+                    displayCustomerInfo(gasType, gallons, totalMoney);
 
-            updateTransactions(gasType, gallons, totalMoney);
+                    updateTransactions(gasType, gallons, totalMoney);
 
-            updatingInventory(gasType, gallons, totalMoney);
+                    updatingInventory(gasType, gallons, totalMoney);
+                }
+            }
         }
     }
-}
